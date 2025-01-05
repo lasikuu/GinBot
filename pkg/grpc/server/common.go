9630db976b012328e94aea61fb4758f6dfaa5f72
Marko Leinikka <mleinikka@gmail.com>
2025-01-06 06:10:23 +0900

    feat: remove user_clearance from metadata

--- pkg/grpc/server/common.go
+++ pkg/grpc/server/common.go
@@ -10,8 +10,7 @@ import (
 )
 
 type UserInfo struct {
-	ID        int64
-	Clearance int8
+	ID int64
 }
 
 func getMetadata(ctx context.Context) (*UserInfo, error) {
@@ -20,10 +19,9 @@ func getMetadata(ctx context.Context) (*UserInfo, error) {
 		return nil, status.Errorf(codes.DataLoss, "failed to get metadata.")
 	}
 	userIDs := md.Get("user_id")
-	clearances := md.Get("user_clearance")
 
-	if len(userIDs) == 0 || len(clearances) == 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "user_id or user_clearance is not provided.")
+	if len(userIDs) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "user_id is not provided.")
 	}
 
 	userID, err := strconv.ParseInt(userIDs[0], 10, 64)
@@ -31,13 +29,7 @@ func getMetadata(ctx context.Context) (*UserInfo, error) {
 		return nil, status.Errorf(codes.InvalidArgument, "failed to parse user_id.")
 	}
 
-	clearance, err := strconv.ParseInt(clearances[0], 10, 8)
-	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "failed to parse user_clearance.")
-	}
-
 	return &UserInfo{
-		ID:        userID,
-		Clearance: int8(clearance),
+		ID: userID,
 	}, nil
 }
