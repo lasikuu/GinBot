808079eca057586d0dd1f0c3c251a67870c8ba76
Marko Leinikka <mleinikka@gmail.com>
2025-01-06 06:10:12 +0900

    feat: implement register

--- pkg/db/user.go
+++ pkg/db/user.go
@@ -1,10 +1,61 @@
 package db
 
 import (
+	"context"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/lasikuu/GinBot/pkg/gen/proto"
+	"github.com/lasikuu/GinBot/pkg/log"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/structpb"
 )
+
+func CreateUser(username string, platform proto.Platform, platformUserId string, platformMetadata *structpb.Struct, locale *string) (*string, error) {
+	userUUID, err := uuid.NewV7()
+	if err != nil {
+		log.Z.Error("failed to generate UUID.", zap.Error(err))
+		return nil, err
+	}
+	userID := userUUID.String()
+
+	_, err = db().Exec(
+		context.Background(),
+		"INSERT INTO \"user\" (id, username, locale) values($1, $2, $3)",
+		userID, username, locale,
+	)
+	if err != nil {
+		log.Z.Error("failed to insert user.", zap.Error(err))
+		return nil, err
+	}
+
+	_, err = db().Exec(
+		context.Background(),
+		"INSERT INTO \"platform_user\" (user_id, platform, platform_user_id, metadata) values($1, $2, $3, $4)",
+		userID, platform, platformUserId, platformMetadata,
+	)
+	if err != nil {
+		log.Z.Error("failed to insert platform user.", zap.Error(err))
+		return nil, err
+	}
+
+	return &userID, err
+}
+
+func GetUser(id string) *proto.User {
+	var user proto.User
+	err := db().QueryRow(
+		context.Background(),
+		"SELECT * FROM \"user\" WHERE id = $1", id,
+	).Scan(&user)
+
+	if err != nil {
+		log.Z.Debug("failed to scan user.", zap.Error(err))
+		return nil
+	}
+
+	return &user
+}
 
 func CongratulableBirthdays(now *time.Time) ([]*proto.Reminder, error) {
 	return nil, nil
--- pkg/grpc/server/user.go
+++ pkg/grpc/server/user.go
@@ -3,7 +3,10 @@ package server
 import (
 	"context"
 
+	"github.com/lasikuu/GinBot/pkg/db"
 	pb "github.com/lasikuu/GinBot/pkg/gen/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -14,6 +17,21 @@ type UserServer struct {
 func NewUserServer() *UserServer {
 	s := &UserServer{}
 	return s
+}
+
+func (s *UserServer) Register(_ context.Context, req *pb.RegisterReq) (*pb.RegisterResp, error) {
+	if req.Username == nil || req.Platform == nil || req.PlatformUserId == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "username, platform, and platform_user_id are required.")
+	}
+
+	userId, err := db.CreateUser(*req.Username, *req.Platform, *req.PlatformUserId, req.PlatformMetadata, req.Locale)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.RegisterResp{
+		UserId: userId,
+	}, nil
 }
 
 func (s *UserServer) GetCongratulableBirthdays(ctx context.Context, _ *emptypb.Empty) (*pb.GetCongratulableBirthdaysResp, error) {
