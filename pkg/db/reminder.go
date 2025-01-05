14a081a2b920cdb818fc6ceecbbd2edda8cf6c00
Marko Leinikka <mleinikka@gmail.com>
2025-01-05 15:08:45 +0900

    feat: add stubs for ExpiredReminders and ListReminders db handlers

--- pkg/db/reminder.go
+++ pkg/db/reminder.go
@@ -1,7 +1,26 @@
 package db
 
-import "github.com/lasikuu/GinBot/pkg/gen/proto"
+import (
+	"time"
+
+	"github.com/lasikuu/GinBot/pkg/gen/proto"
+)
 
 func GetReminder(id *int64, userID *int64) (*proto.Reminder, error) {
+	return nil, nil
+}
+
+func ListReminders(
+	limit *int64,
+	offset *int64,
+	message *string,
+	userID *int64,
+	periodStart *time.Time,
+	periodEnd *time.Time,
+) ([]*proto.Reminder, error) {
+	return nil, nil
+}
+
+func ExpiredReminders(now *time.Time) ([]*proto.Reminder, error) {
 	return nil, nil
 }
