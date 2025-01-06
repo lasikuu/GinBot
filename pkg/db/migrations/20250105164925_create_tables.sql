0c2d6aa4cfc7ac84416e261e626f0e904de0f2e7
Marko Leinikka <mleinikka@gmail.com>
2025-01-07 01:40:53 +0900

    feat: rethink sql

--- pkg/db/migrations/20250105164925_create_tables.sql
+++ pkg/db/migrations/20250105164925_create_tables.sql
@@ -36,50 +36,49 @@ CREATE TABLE IF NOT EXISTS "user_account"
 );
 COMMENT ON TABLE user_account IS 'internal user accounts';
 
-CREATE TABLE IF NOT EXISTS "platform"
+CREATE TABLE IF NOT EXISTS "instance"
 (
     id              bigserial UNIQUE NOT NULL,
     platform_enum   int              NOT NULL,
-    platform_meta   jsonb            NOT NULL,
+    instance_meta   jsonb            NOT NULL,
     default_channel text,
     deleted         boolean          NOT NULL DEFAULT FALSE,
     created_at      timestamp        NOT NULL DEFAULT NOW(),
     updated_at      timestamp        NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
-COMMENT ON TABLE platform IS 'platforms linked to the bot';
-COMMENT ON COLUMN "platform".platform_enum IS '0=unknown, 1=matrix protocol, 2=discord, 3=telegram, 4=line, 5=email, 6=snailmail';
-COMMENT ON COLUMN "platform".platform_meta IS 'platform specific metadata (server id etc.)';
-COMMENT ON COLUMN "platform".default_channel IS 'default communication channel for the platform for announcements etc.';
+COMMENT ON TABLE instance IS 'instances linked to the bot';
+COMMENT ON COLUMN instance.platform_enum IS '0=unknown, 1=matrix protocol, 2=discord, 3=telegram, 4=line, 5=email, 6=snailmail';
+COMMENT ON COLUMN instance.instance_meta IS 'instance specific metadata (server id etc.)';
+COMMENT ON COLUMN instance.default_channel IS 'default communication channel for the instance for announcements etc.';
 
-CREATE INDEX idx_platform_category ON "platform" (platform_enum);
+CREATE INDEX idx_instance_platform_enum ON instance (platform_enum);
 
 CREATE TABLE IF NOT EXISTS "platform_user"
 (
     id            bigserial UNIQUE NOT NULL,
-    platform_id   bigint           NOT NULL,
+    user_id       uuid             NOT NULL,
+    platform_enum int              NOT NULL,
     platform_uid  text             NOT NULL,
-    platform_meta jsonb,
-    user_id       uuid,
+    user_meta     jsonb,
     deleted       boolean          NOT NULL DEFAULT FALSE,
     created_at    timestamp        NOT NULL DEFAULT NOW(),
     updated_at    timestamp        NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_platform_user_platform FOREIGN KEY (platform_id) REFERENCES "platform" (id),
-    CONSTRAINT fk_platform_message_user FOREIGN KEY (user_id) REFERENCES "user_account" (id)
+    CONSTRAINT fk_user_account FOREIGN KEY (user_id) REFERENCES "user_account" (id),
+    CONSTRAINT unique_platform_user_enum_uid UNIQUE (platform_enum, platform_uid)
 );
 COMMENT ON TABLE platform_user IS 'platform specific user data';
-COMMENT ON COLUMN "platform_user".platform_id IS 'id of the platform';
 COMMENT ON COLUMN "platform_user".platform_uid IS 'id of the user on the platform';
-COMMENT ON COLUMN "platform_user".platform_meta IS 'platform specific metadata (display name, roles etc.)';
+COMMENT ON COLUMN "platform_user".user_meta IS 'platform specific metadata (display name, roles etc.)';
 
-CREATE INDEX idx_platform_user_platform ON "platform_user" (platform_id);
+CREATE INDEX idx_platform_user_platform_enum ON "platform_user" (platform_enum);
 
-CREATE TABLE IF NOT EXISTS "platform_message"
+CREATE TABLE IF NOT EXISTS "message"
 (
     id            bigserial UNIQUE NOT NULL,
-    platform_id   bigint           NOT NULL,
-    platform_meta jsonb            NOT NULL,
+    instance_id   bigint           NOT NULL,
+    msg_meta      jsonb            NOT NULL,
     msg_content   text             NOT NULL,
     msg_timestamp timestamp        NOT NULL,
     user_id       uuid,
@@ -87,44 +86,44 @@ CREATE TABLE IF NOT EXISTS "platform_message"
     created_at    timestamp        NOT NULL DEFAULT NOW(),
     updated_at    timestamp        NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_platform_message_platform FOREIGN KEY (platform_id) REFERENCES "platform" (id),
-    CONSTRAINT fk_platform_message_user FOREIGN KEY (user_id) REFERENCES "user_account" (id)
+    CONSTRAINT fk_message_instance FOREIGN KEY (instance_id) REFERENCES "instance" (id),
+    CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES "user_account" (id)
 );
-COMMENT ON TABLE platform_message IS 'messages on any platform';
-COMMENT ON COLUMN "platform_message".platform_meta IS 'metadata of the message including the msg id, channel id etc.';
-COMMENT ON COLUMN "platform_message".user_id IS 'internal user_id of the msg author if available';
+COMMENT ON TABLE message IS 'messages on any platform';
+COMMENT ON COLUMN message.msg_meta IS 'metadata of the message including the uid msg_id, channel_id etc.';
+COMMENT ON COLUMN message.user_id IS 'internal user_id of the msg author if available';
 
-CREATE INDEX idx_platform_message_platform ON "platform_message" (platform_id);
+CREATE INDEX idx_message_instance ON "message" (instance_id);
 
-CREATE TABLE IF NOT EXISTS "platform_attachment"
+CREATE TABLE IF NOT EXISTS "message_attachment"
 (
-    id                  bigserial UNIQUE NOT NULL,
-    platform_message_id bigint           NOT NULL,
-    file_id             uuid,
-    file_url            text,
-    deleted             boolean          NOT NULL DEFAULT FALSE,
-    created_at          timestamp        NOT NULL DEFAULT NOW(),
-    updated_at          timestamp        NOT NULL DEFAULT NOW(),
+    id         bigserial UNIQUE NOT NULL,
+    message_id bigint           NOT NULL,
+    file_id    uuid,
+    file_url   text,
+    deleted    boolean          NOT NULL DEFAULT FALSE,
+    created_at timestamp        NOT NULL DEFAULT NOW(),
+    updated_at timestamp        NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_platform_attachment_message FOREIGN KEY (platform_message_id) REFERENCES "platform_message" (id) ON DELETE CASCADE,
-    CONSTRAINT fk_platform_attachment_file FOREIGN KEY (file_id) REFERENCES "file" (id)
+    CONSTRAINT fk_message_attachment_msg FOREIGN KEY (message_id) REFERENCES "message" (id) ON DELETE CASCADE,
+    CONSTRAINT fk_message_attachment_file FOREIGN KEY (file_id) REFERENCES "file" (id)
 );
-COMMENT ON TABLE platform_attachment IS 'attachments of a message';
+COMMENT ON TABLE message_attachment IS 'attachments of a message';
 
 CREATE TABLE IF NOT EXISTS "destination"
 (
-    id            bigserial UNIQUE NOT NULL,
-    platform_id   bigint           NOT NULL,
-    platform_meta jsonb            NOT NULL,
-    deleted       boolean          NOT NULL DEFAULT FALSE,
-    created_at    timestamp        NOT NULL DEFAULT NOW(),
-    updated_at    timestamp        NOT NULL DEFAULT NOW(),
+    id          bigserial UNIQUE NOT NULL,
+    instance_id bigint           NOT NULL,
+    meta        jsonb            NOT NULL,
+    deleted     boolean          NOT NULL DEFAULT FALSE,
+    created_at  timestamp        NOT NULL DEFAULT NOW(),
+    updated_at  timestamp        NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_destination_platform FOREIGN KEY (platform_id) REFERENCES "platform" (id) ON DELETE CASCADE
+    CONSTRAINT fk_destination_instance FOREIGN KEY (instance_id) REFERENCES "instance" (id) ON DELETE CASCADE
 );
 COMMENT ON TABLE destination IS 'message destinations (channels, rooms etc.)';
 
-CREATE INDEX idx_destination_platform ON "destination" (platform_id);
+CREATE INDEX idx_destination_instance ON "destination" (instance_id);
 
 CREATE TABLE IF NOT EXISTS "reminder"
 (
@@ -201,8 +200,8 @@ CREATE TABLE IF NOT EXISTS "highlight"
     created_at   timestamp   NOT NULL DEFAULT NOW(),
     updated_at   timestamp   NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
-    CONSTRAINT fk_highlight_original_msg FOREIGN KEY (original_mid) REFERENCES "platform_message" (id),
-    CONSTRAINT fk_highlight_copy_msg FOREIGN KEY (copy_mid) REFERENCES "platform_message" (id)
+    CONSTRAINT fk_highlight_original_msg FOREIGN KEY (original_mid) REFERENCES "message" (id),
+    CONSTRAINT fk_highlight_copy_msg FOREIGN KEY (copy_mid) REFERENCES "message" (id)
 );
 COMMENT ON TABLE highlight IS 'highlighted or pinned messages';
 COMMENT ON COLUMN "highlight".original_mid IS 'id of the original message';
@@ -211,8 +210,8 @@ COMMENT ON COLUMN "highlight".copy_mid IS 'id of the message copy (e.g. message 
 CREATE TABLE IF NOT EXISTS "linked"
 (
     id            BIGSERIAL UNIQUE NOT NULL,
-    platform_id   bigint           NOT NULL,
-    platform_meta jsonb            NOT NULL,
+    instance_id   bigint           NOT NULL,
+    instance_meta jsonb            NOT NULL,
     category      int              NOT NULL,
     url           text,
     file_id       uuid,
@@ -225,7 +224,7 @@ CREATE TABLE IF NOT EXISTS "linked"
     CONSTRAINT chk_reply_or_file CHECK (url IS NOT NULL OR file_id IS NOT NULL)
 );
 COMMENT ON TABLE linked IS 'sent links and files to determine if they have already been sent';
-COMMENT ON COLUMN "linked".platform_meta IS 'platform specific metadata (user id, msg id, channel id etc.)';
+COMMENT ON COLUMN "linked".instance_meta IS 'instance specific metadata (user id, msg id, channel id etc.)';
 COMMENT ON COLUMN "linked".category IS '0=unknown, 1=url, 2=file';
 
 CREATE INDEX idx_linked_category ON "linked" (category);
@@ -237,10 +236,10 @@ CREATE INDEX idx_linked_category ON "linked" (category);
 DROP INDEX IF EXISTS idx_linked_category;
 DROP INDEX IF EXISTS idx_reminder_status;
 DROP INDEX IF EXISTS idx_reminder_datetime;
-DROP INDEX IF EXISTS idx_destination_platform;
-DROP INDEX IF EXISTS idx_platform_message_platform;
+DROP INDEX IF EXISTS idx_destination_instance;
+DROP INDEX IF EXISTS idx_message_instance;
 DROP INDEX IF EXISTS idx_platform_user_platform;
-DROP INDEX IF EXISTS idx_platform_category;
+DROP INDEX IF EXISTS idx_instance_platform_enum;
 DROP INDEX IF EXISTS idx_file_category;
 
 DROP TABLE IF EXISTS "linked";
@@ -250,10 +249,10 @@ DROP TABLE IF EXISTS "trigger";
 DROP TABLE IF EXISTS "destination_preference";
 DROP TABLE IF EXISTS "reminder";
 DROP TABLE IF EXISTS "destination";
-DROP TABLE IF EXISTS "platform_attachment";
-DROP TABLE IF EXISTS "platform_message";
+DROP TABLE IF EXISTS "message_attachment";
+DROP TABLE IF EXISTS "message";
 DROP TABLE IF EXISTS "platform_user";
-DROP TABLE IF EXISTS "platform";
+DROP TABLE IF EXISTS "instance";
 DROP TABLE IF EXISTS "user_account";
 DROP TABLE IF EXISTS "file";
 -- +goose StatementEnd
