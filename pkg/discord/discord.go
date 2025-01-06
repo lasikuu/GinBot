9771b299ca66e0a369e9a5dc2890e9d7f15d22bf
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2025-01-06 19:22:58 +0200

    feat(discord): add handlers for message component interactions

--- pkg/discord/discord.go
+++ pkg/discord/discord.go
@@ -23,9 +23,17 @@ func InitializeDiscord() {
 	})
 
 	discordSession.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+				h(s, i)
+			}
+		case discordgo.InteractionMessageComponent:
+			if h, ok := componentHandlers[i.MessageComponentData().CustomID]; ok {
+				h(s, i)
+			}
 		}
+
 	})
 
 	if err = discordSession.Open(); err != nil {
